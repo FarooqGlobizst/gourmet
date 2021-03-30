4a8fb2f53d59667ef2076d64b2c7c7ea8130722d
Farooq Akram <globizst.ent@gmail.com>
2021-03-31 01:54:55 +0800

    database connection and dynameics products

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+  $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_cruds;', 'root','');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $staement = $pdo->prepare('SELECT * FROM products ORDER BY date_created DESC');
+  $staement->execute();
+  $products = $staement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,11 +18,45 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Products</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
-
+  
+  <div class="container">
+    <h1>Products</h1>
+    <a href="create.php" class="btn btn-primary">Create Product</a>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Item No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
+          <th scope="col">Price</th>
+          <th scope="col">Created Date</th>
+          <th scope="col">Image</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach($products as $i => $product){ ?>
+        <tr>
+          <th scope="row"><?php echo $i+1 ?></th>
+          <td><?php echo $product['title'] ?></td>
+          <td><?php echo $product['description'] ?></td>
+          <td><?php echo $product['price'] ?></td>
+          <td><?php echo $product['date_created'] ?></td>
+          <td><img src="<?php echo $product['image'] ?>" width="100px;" alt=""></td>
+          <td>
+            <a href="edit.php" class="btn btn-success">Edit</a>
+            <a href="delete.php" class="btn btn-danger">Delete</a>
+          </td>
+          
+        </tr>
+        <?php } ?>
+        
+      </tbody>
+    </table>
+  </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
