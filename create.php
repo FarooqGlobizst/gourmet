dc7ff198d54b71cde0a0f342a21040631d5bebf8
Farooq Akram <globizst.ent@gmail.com>
2021-03-31 13:26:12 +0800

    database binding with create.php

--- create.php
+++ create.php
@@ -1,3 +1,30 @@
+<?php
+  $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_cruds;', 'root','');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  //declaring variables
+    if(isset($_GET['submit'])){
+        $title = $_GET['title'];
+        $discription = $_GET['discription'];
+        $price = $_GET['price'];
+        $date = date('Y-m-d H:m:i');
+
+     //database binding and query
+        $statement = $pdo->prepare(
+            "INSERT INTO products (title, description, image, price, date_created )
+             VALUES(:title, :discription, :image,  :price, :date)");
+
+             $statement->bindValue(':title', $title);
+             $statement->bindValue(':discription', $discription);
+             $statement->bindValue(':image', '');
+             $statement->bindValue(':price', $price);
+             $statement->bindValue(':date', $date);
+             $statement->execute();
+
+    }
+
+  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,28 +39,28 @@
 <body>
 <div class="container">
 <h1>Create Product</h1>
-    <form class="col-sm-4">
+    <form class="col-sm-4" action="" method="get">
         <div class="form-group">
             <label>Image Upload</label>
-            <input type="file" class="form-control" >
+            <input type="file" class="form-control" name="image" >
         </div>
 
         <div class="form-group">
             <label>Product Title</label>
-            <input type="text" class="form-control" >
+            <input type="text" class="form-control" name="title">
         </div>
 
         <div class="form-group">
             <label>Product Description</label>
-            <textarea class="form-control" ></textarea>
+            <textarea class="form-control" name="discription"></textarea>
         </div>
 
         <div class="form-group">
             <label>Product Price</label>
-            <input type="number" step=".01" class="form-control" >
+            <input type="number" step=".01" class="form-control" name="price">
         </div>
         
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
     
